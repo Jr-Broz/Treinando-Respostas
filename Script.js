98b7db6c27bc556fc07d68d1ab1895f175005414
João Rafael Broz dos Santos <127759756+Jr-Broz@users.noreply.github.com>
2023-05-08 22:35:24 -0300

    Update and rename .gitattributes to Script.js

---
+++ Script.js
@@ -0,0 +1,11 @@
+let nomeProprio = "Pedro"
+let nome = prompt("Qual seu nome?");
+let quer = prompt(nome + "," + " Você quer comer Pizza?");
+if(quer === "Sim" || quer === "sim"){
+
+  document.write("Uma para mim" , " e uma para " + nomeProprio)
+}
+  else if(quer === "Não" || quer === "nao" || quer === 'não'){
+
+    document.write("Ok," + nome + " Entendo não querer comer agora. ")
+  }
